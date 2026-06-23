00520860af30e363cab42a23d51457895cf8e1ad
shotaro ishida <ishida.shotaro.1395@icloud.com>
2026-06-24 07:40:04 +0900

    Initial Commit

---
+++ test02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test02
+//
+//  Created by Shotaro Ishida on 2026/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test02/test02App.swift
@@ -0,0 +1,17 @@
+//
+//  test02App.swift
+//  test02
+//
+//  Created by Shotaro Ishida on 2026/06/24.
+//
+
+import SwiftUI
+
+@main
+struct test02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
